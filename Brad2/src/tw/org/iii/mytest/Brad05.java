248c2c792aba22c31bbf6fa5bfb7ff05ec675b08
Administrator <Administrator@AIO-2101>
2018-07-22 14:42:30 +0800

    20180722pm2

---
+++ Brad2/src/tw/org/iii/mytest/Brad05.java
@@ -0,0 +1,23 @@
+package tw.org.iii.mytest;
+
+public class Brad05 {
+
+	public static void main(String[] args) {
+		char c1 = 'a';
+		System.out.println(c1);
+		char c2 = '資';
+		System.out.println(c2);
+		char c3 = 65;	// 0 - 65535
+		System.out.println(c3);	// ASCII code
+		char c4 = '\u0041';
+		System.out.println(c4);	// ASCII code
+		
+		System.out.println(0x41);
+		
+		int a1 = c1 + 4;
+		System.out.println(a1);
+		
+		
+	}
+
+}
