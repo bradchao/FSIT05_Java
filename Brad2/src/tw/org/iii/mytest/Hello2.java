7719c992adb32d841264d6e3d6dd1143a995d119
Administrator <Administrator@AIO-2101>
2018-07-22 11:50:47 +0800

    init

---
+++ Brad2/src/tw/org/iii/mytest/Hello2.java
@@ -0,0 +1,9 @@
+package tw.org.iii.mytest;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello2");
+	}
+
+}
---
+++ Brad2/src/tw/org/iii/mytest/HelloWorld.java
@@ -0,0 +1,7 @@
+package tw.org.iii.mytest;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, Brad");
+	}
+}
